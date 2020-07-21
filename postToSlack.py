9ed43eedd130b2ec8669755996373bf3c3a4b6bd
Hiroaki-K4 <49no19@gmail.com>
2020-07-21 22:53:02 +0900

    make

--- postToSlack.py
+++ postToSlack.py
@@ -43,12 +43,14 @@ def main():
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
         }
-    url_name = "https://openaccess.thecvf.com/CVPR2020"
+    url_name = "https://openaccess.thecvf.com/CVPR2020?day=2020-06-16"
     url = requests.get(url_name, headers)
     soup = BeautifulSoup(url.content, "html.parser")
     elems = soup.find_all("dt")
     for elem in tqdm(elems):
         url = elem.next.next.attrs['href']
+        # url = url.replace('html', 'papers')
+        # url = url.replace('.papers', '.html')
         ori_link = "https://openaccess.thecvf.com/"
         contents_url = ori_link + url
         paper_link = requests.get(contents_url, headers)
@@ -74,7 +76,87 @@ def main():
             slack.notify(attachments=attachments_title)
             slack.notify(attachments=attachments_contents)
         else:
-            slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+            slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+            slack.notify(attachments=attachments_title)
+            slack.notify(attachments=attachments_contents)
+
+        time.sleep(3600)
+
+    
+    url_name = "https://openaccess.thecvf.com/CVPR2020?day=2020-06-17"
+    url = requests.get(url_name, headers)
+    soup = BeautifulSoup(url.content, "html.parser")
+    elems = soup.find_all("dt")
+    for elem in tqdm(elems):
+        url = elem.next.next.attrs['href']
+        # url = url.replace('html', 'papers')
+        # url = url.replace('.papers', '.html')
+        ori_link = "https://openaccess.thecvf.com/"
+        contents_url = ori_link + url
+        paper_link = requests.get(contents_url, headers)
+        paper_soup = BeautifulSoup(paper_link.content, "html.parser")
+        title = paper_soup.find('div', id="papertitle").text
+        abstract = paper_soup.find('div', id="abstract").text
+        abstract_ja = translator.translate(abstract, src='en', dest='ja')
+        abstract_ja = str(abstract_ja.text)
+        pdf = paper_soup.find("dd").find("a").attrs['href'][6:]
+        pdf_link = ori_link + pdf
+        class_name = sort_class(abstract)
+
+        attachments_title = []
+        attachments_contents = []
+        paper_title = {"title": title,
+                "text": pdf_link}
+        paper_contents = {"title": abstract,
+                "text": abstract_ja}
+        attachments_title.append(paper_title)
+        attachments_contents.append(paper_contents)
+        if class_name == "adver":
+            slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+            slack.notify(attachments=attachments_title)
+            slack.notify(attachments=attachments_contents)
+        else:
+            slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+            slack.notify(attachments=attachments_title)
+            slack.notify(attachments=attachments_contents)
+
+        time.sleep(3600)
+
+
+    url_name = "https://openaccess.thecvf.com/CVPR2020?day=2020-06-18"
+    url = requests.get(url_name, headers)
+    soup = BeautifulSoup(url.content, "html.parser")
+    elems = soup.find_all("dt")
+    for elem in tqdm(elems):
+        url = elem.next.next.attrs['href']
+        # url = url.replace('html', 'papers')
+        # url = url.replace('.papers', '.html')
+        ori_link = "https://openaccess.thecvf.com/"
+        contents_url = ori_link + url
+        paper_link = requests.get(contents_url, headers)
+        paper_soup = BeautifulSoup(paper_link.content, "html.parser")
+        title = paper_soup.find('div', id="papertitle").text
+        abstract = paper_soup.find('div', id="abstract").text
+        abstract_ja = translator.translate(abstract, src='en', dest='ja')
+        abstract_ja = str(abstract_ja.text)
+        pdf = paper_soup.find("dd").find("a").attrs['href'][6:]
+        pdf_link = ori_link + pdf
+        class_name = sort_class(abstract)
+
+        attachments_title = []
+        attachments_contents = []
+        paper_title = {"title": title,
+                "text": pdf_link}
+        paper_contents = {"title": abstract,
+                "text": abstract_ja}
+        attachments_title.append(paper_title)
+        attachments_contents.append(paper_contents)
+        if class_name == "adver":
+            slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+            slack.notify(attachments=attachments_title)
+            slack.notify(attachments=attachments_contents)
+        else:
+            slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
             slack.notify(attachments=attachments_title)
             slack.notify(attachments=attachments_contents)
 
